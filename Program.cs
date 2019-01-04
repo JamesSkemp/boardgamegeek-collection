5f7613fd35f91e486ed78009f6a8528452f1e2cd
James Skemp <strivinglife@gmail.com>
2019-01-03 18:07:32 -0600

    Create new .NET Core 2.2 console application

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace boardgamegeek_collection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
