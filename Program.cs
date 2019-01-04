229d3fe5c65f1321314a71569b5df212e21f4370
James Skemp <strivinglife@gmail.com>
2019-01-03 20:06:48 -0600

    Add start of functionality to pull user's collection

--- Program.cs
+++ Program.cs
@@ -1,12 +1,57 @@
 ﻿using System;
+using System.IO;
+using System.Net.Http;
+using Microsoft.Extensions.Configuration;
 
 namespace boardgamegeek_collection
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+	class Program
+	{
+		private static IConfigurationRoot configuration;
+
+		static void Main(string[] args)
+		{
+			var builder = new ConfigurationBuilder()
+				.SetBasePath(Directory.GetCurrentDirectory())
+				.AddJsonFile("appsettings.json");
+
+			configuration = builder.Build();
+
+			Console.WriteLine(configuration["apiBaseUrl"]);
+
+			GetData();
+
+			Console.WriteLine("Hello World!");
+
+			Console.ReadLine();
+			Console.ReadKey();
+		}
+
+		static async void GetData()
+		{
+			string baseUrl = $"{configuration["apiBaseUrl"]}collection/{configuration["defaultUser"]}";
+			Console.WriteLine(baseUrl);
+			using (HttpClient client = new HttpClient())
+			{
+				using (HttpResponseMessage res = await client.GetAsync(baseUrl))
+				{
+					if (res.StatusCode == System.Net.HttpStatusCode.OK)
+					{
+						using (HttpContent content = res.Content)
+						{
+							string data = await content.ReadAsStringAsync();
+							if (data != null)
+							{
+								Console.WriteLine(data);
+							}
+						}
+					}
+					else
+					{
+						Console.WriteLine(res.StatusCode.ToString());
+					}
+				}
+			}
+		}
+	}
 }
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# boardgamegeek-collection
-.NET Core application to pull a user's collection from Board Game Geek.
+# BoardGameGeek Collection Data
+[].NET Core 2] application to pull a user's collection from [BoardGameGeek][bgg].
+
+This uses the [BGG XML API][bgg-xml-api] to pull information.
+
+## Building the Application
+> TODO
+
+## Running the Application
+> TODO
+
+[bgg]: https://boardgamegeek.com/
+[bgg-xml-api]: https://boardgamegeek.com/wiki/page/BGG_XML_API
